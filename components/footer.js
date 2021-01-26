d94fe022e19699b6bb917a170ad9f65476fddf48
altegani mohammed <tiganimoh@gmail.com>
2021-01-26 09:50:34 +0200

    add background header

--- components/footer.js
+++ components/footer.js
@@ -11,18 +11,17 @@ export default ()=>{
         <Divider hidden section />
           <Grid.Row>
             <Grid.Column>
-              <Divider />
-              <footer style={{color:'blue',textAlign:'center',fontSize: '1.1em'}}>
+              <footer style={{color:'blue',textAlign:'center',fontSize: '1.1em',
+              backgroundColor:'#f5f5f5',marginTop:'0',marginBottom:'0'}}>
                 Gulf Human Capital Society
-              </footer>
-            </Grid.Column>
-          </Grid.Row>
-          <Grid.Row>
+                <br/>
            <Grid.Column >
            <Button circular color='facebook' icon='facebook' />
            <Button circular color='twitter' icon='twitter' />
            <Button circular color='linkedin' icon='linkedin' />
            <Button circular color='google plus' icon='google plus' />
+            </Grid.Column>
+              </footer>
             </Grid.Column>
           </Grid.Row>
         </Grid>
